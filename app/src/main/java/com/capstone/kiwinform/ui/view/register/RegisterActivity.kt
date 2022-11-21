456573854d759102cbd538aa99389066cd7c1d0d
Nabil Nufail <mnabilnuf@gmail.com>
2022-11-21 17:05:08 +0700

    first commit

---
+++ app/src/main/java/com/capstone/kiwinform/ui/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.kiwinform.ui.view.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.kiwinform.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/capstone/kiwinform/ui/view/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.kiwinform.ui.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.kiwinform.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
