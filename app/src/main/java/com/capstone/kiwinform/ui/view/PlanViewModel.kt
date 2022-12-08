90fb0bbd3e0b0a17e0ef8263da81a0ac1a4aa804
Brahma Aditama <aditama.brahma@gmail.com>
2022-12-08 12:18:05 +0000

    Merge ke main

---
+++ app/src/main/java/com/capstone/kiwinform/ui/view/PlanViewModel.kt
@@ -0,0 +1,35 @@
+package com.capstone.kiwinform.ui.view
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PlanViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: PlanRepository
+    val listAllPlan: LiveData<List<Plan>>
+    val todaysPlan: LiveData<List<Plan>>
+
+    init {
+        val planDao = PlanDatabase.getDatabase(application).planDao()
+        repository = PlanRepository(planDao)
+
+        listAllPlan = repository.getListPlan
+
+        todaysPlan = repository.getTodaysPlan
+    }
+
+    fun insertNotes(plan: Plan) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertPlan(plan)
+    }
+
+    fun updateNotes(plan: Plan) = viewModelScope.launch(Dispatchers.IO){
+        repository.updatePlan(plan)
+    }
+
+    fun delete(id: Int) = viewModelScope.launch(Dispatchers.IO) {
+        repository.deletePlan(id)
+    }
+}
---
+++ app/src/main/java/com/capstone/kiwinform/ui/view/dashboard/HomeFragment.kt
@@ -0,0 +1,63 @@
+package com.capstone.kiwinform.ui.view.dashboard
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.kiwinform.databinding.FragmentHomeBinding
+import com.capstone.kiwinform.ui.view.ListPlanAdapter
+import com.capstone.kiwinform.ui.view.Plan
+import com.capstone.kiwinform.ui.view.PlanViewModel
+import com.capstone.kiwinform.ui.view.reminder.ReminderActivity
+
+class HomeFragment : Fragment() {
+    private var _binding : FragmentHomeBinding?= null
+    private val binding get() = _binding!!
+    private lateinit var planViewModel: PlanViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listPlanAdapter = ListPlanAdapter()
+        binding.rvTodaysActivities.setHasFixedSize(true)
+        binding.rvTodaysActivities.layoutManager = LinearLayoutManager(activity)
+        binding.rvTodaysActivities.adapter = listPlanAdapter
+
+        planViewModel = ViewModelProvider(this).get(PlanViewModel::class.java)
+        planViewModel.todaysPlan.observe(viewLifecycleOwner, {
+            if (it != null) {
+                listPlanAdapter.setList(it)
+            }
+        })
+
+        listPlanAdapter.setOnItemClickCallback(object : ListPlanAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Plan) {
+                showSelectedPlan(data)
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showSelectedPlan(plan: Plan) {
+        val moveIntent = Intent(context, ReminderActivity::class.java)
+        moveIntent.putExtra(ReminderActivity.EXTRA_PLAN, plan)
+        activity?.startActivityForResult(moveIntent, ReminderActivity.REQUEST_CODE_ADD)
+    }
+}
